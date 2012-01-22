f60bfeaf026a7f28cc3d5b20b08ea24ffc0ab6e0
Marcel Massana <xaxaupua@gmail.com>
2012-01-22 23:17:15 +0100

    finalitzat Cap3, inclosos un parell d'exercicis del final

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ RorTutorial::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
 
   resources :microposts
 
--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
+
+
   test "should get home" do
     get :home
     assert_response :success
@@ -18,7 +25,7 @@ class PagesControllerTest < ActionController::TestCase
 
   test "contact should have the right title" do
     get :contact
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Contact"
+    assert_select "title", @base_title+"Contact"
   end
 
   test "should get about" do
@@ -28,7 +35,18 @@ class PagesControllerTest < ActionController::TestCase
 
   test "about should have the right title" do
     get :about
-    assert_select "title", "Ruby on Rails Tutorial Sample App | About"
+    assert_select "title", @base_title+"About"
   end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+  test "help should have the right title" do
+    get :help
+    assert_select "title", @base_title+"Help"
+  end
+
 
 end
