53123d861fea26e8be5030a954b8a02b8876cd71
Marcel Massana <xaxaupua@gmail.com>
2012-01-29 01:42:03 +0100

    fet helper per a manegar gravatars i en el seu defecte avatars aleatoris d'un directori

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
 		image_tag("logo.png", :alt => "Sample App", :class => "round")
 	end
 
+	# Dona informacio formatejada de les expessions que se li posin en una llista de strings separada per comes
+	# Per exemple:
+	# 	print_debug_expressions "image_path('/googleeyes.png')", "url_for 'assets/images/googleeyes.png'", "root_url+url_for('images/googleeyes.png')"
+	def print_debug_expressions *args
+		out = "<strong>PRINT DEBUG EXPRESSIONS</strong><br />"
+		args.each do |arg|
+			out << "#{arg.to_s} ==> <strong>#{eval(arg).to_s}</strong><br />"
+		end 
+		raw(out)	# MME des de Rails 3 la sortida s'escapeja amb h() per defecte, i per tant, s'ha de evitar aixo amb raw()
+	end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,26 @@
 module UsersHelper
 
-  def gravatar_for(user, options = { :size => 100 })
-    gravatar_image_tag user.email.downcase, :alt => h(user.name),
-                                            :class => 'gravatar round',
-                                            :gravatar => options
+  # 
+  def gravatar_for(user, options = {})
+ 	options ||={}
+  	default_img_url=root_url+url_for("assets/avatars/#{random_avatar}")
+ 	opts = { :default => default_img_url , :size => 50 }
+  	opts.merge! options
+  	options = {
+  		:alt => h(user.name),
+  		:class => 'gravatar round',
+  		:gravatar => opts
+  	}
+	options.merge!({:heigth=>opts[:size], :width=>opts[:size]}) if opts.has_key? :size
+    gravatar_image_tag user.email.downcase, options
   end
+
+  private
+
+  	  # chooses a random image (png, jpg or gif) from .../avatars directory
+	  def random_avatar
+	  	avatars_dir = File.join(Rails.configuration.root.to_s,"/app/assets/images/avatars/")
+		Dir["#{avatars_dir}*{png,jpg,gif}"].map {|f| f.sub!(avatars_dir,"")}.sample
+	  end
 
 end
